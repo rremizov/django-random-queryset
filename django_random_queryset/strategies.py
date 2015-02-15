6233e9e8c3847f18749fceb1c8c9531585ca7579
rremizov <rremizov@yandex.ru>
2015-02-15 07:42:14 +0500

    selection strategies

--- django_random_queryset/strategies.py
+++ django_random_queryset/strategies.py
@@ -3,7 +3,8 @@
 import random
 
 
-def strategy0(self, amount, available_ids):
+def brute_force(amount, available_ids):
+
     if amount < 1:
         raise ValueError("'amount' is a positive integer value")
 
@@ -19,5 +20,62 @@ def strategy0(self, amount, available_ids):
     return selected_ids
 
 
-DEFAULT = strategy0
+def index_selection(amount, available_ids):
+    """
+    Slow with (amount / len(available_ids)) close to 1.
+    """
+
+    if amount < 1:
+        raise ValueError("'amount' is a positive integer value")
+
+    available_ids_length = len(available_ids)
+
+    if amount > available_ids_length:
+        return available_ids
+
+    selected_indexes = set()
+
+    while len(selected_indexes) < amount:
+        selected_indexes.add(random.randint(0, available_ids_length - 1))
+
+    return {available_ids[index] for index in selected_indexes}
+
+
+def index_exclusion(amount, available_ids):
+    """
+    For cases with (amount / len(available_ids)) close to 1.
+    """
+
+    if amount < 1:
+        raise ValueError("'amount' is a positive integer value")
+
+    available_ids_length = len(available_ids)
+
+    if amount > available_ids_length:
+        return available_ids
+
+    excluded_indexes = set()
+    amount_to_exclude = available_ids_length - amount
+
+    while len(excluded_indexes) < amount_to_exclude:
+        excluded_indexes.add(random.randint(0, available_ids_length - 1))
+
+    selected_ids = set(available_ids)
+
+    for index in excluded_indexes:
+        selected_ids.remove(available_ids[index])
+
+    return selected_ids
+
+
+def index_combo(amount, available_ids):
+
+    if float(amount) / len(available_ids) < 0.5:
+        return index_selection(amount, available_ids)
+
+    else:
+        return index_exclusion(amount, available_ids)
+
+
+DEFAULT = index_combo
 
