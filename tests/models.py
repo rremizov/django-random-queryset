2a22970b035d3675ed59c050f0288a98be681dcc
rremizov <rremizov@yandex.ru>
2015-02-15 07:42:51 +0500

    tests for different selection strategies

--- tests/models.py
+++ tests/models.py
@@ -2,13 +2,16 @@ import uuid
 
 from django.db import models
 
-from django_random_queryset.managers import RandomManager
+from django_random_queryset import managers, strategies
 
 
 class ModelA(models.Model):
     uuid = models.CharField(default=lambda: str(uuid.uuid4()), max_length=128)
 
-    objects = RandomManager()
+    brute_force = managers.RandomManager(strategy=strategies.brute_force)
+    index_selection = managers.RandomManager(strategy=strategies.index_selection)
+    index_exclusion = managers.RandomManager(strategy=strategies.index_exclusion)
+    index_combo = managers.RandomManager(strategy=strategies.index_combo)
 
     def __str__(self):
         return self.uuid
--- tests/tests.py
+++ tests/tests.py
@@ -6,9 +6,19 @@ from .models import ModelA
 class TestMain(TestCase):
     fixtures = ['basic']
 
-    def test_main(self):
-        self.assertEqual(ModelA.objects.random(5).count(), 5)
+    def test_brute_force(self):
+        self.assertEqual(ModelA.brute_force.random(5).count(), 5)
+        self.assertEqual(ModelA.brute_force.random(25).count(), 20)
 
-        self.assertEqual(ModelA.objects.count(), 20)
-        self.assertEqual(ModelA.objects.random(25).count(), 20)
+    def test_index_selection(self):
+        self.assertEqual(ModelA.index_selection.random(5).count(), 5)
+        self.assertEqual(ModelA.index_selection.random(25).count(), 20)
+
+    def test_index_exclusion(self):
+        self.assertEqual(ModelA.index_exclusion.random(5).count(), 5)
+        self.assertEqual(ModelA.index_exclusion.random(25).count(), 20)
+
+    def test_index_combo(self):
+        self.assertEqual(ModelA.index_combo.random(5).count(), 5)
+        self.assertEqual(ModelA.index_combo.random(25).count(), 20)
 
