0adb09934b01ff9a65acf4b53f7921d8645a2d2b
rremizov <rremizov@yandex.ru>
2015-05-16 17:53:10 +0500

    update

--- README.rst
+++ README.rst
@@ -7,7 +7,7 @@ The extension gives you ability to pull random records using Django's ORM.
 Requirements
 ------------
 
-Python 2.7 or 3.2, Django 1.5+.
+Python 2.7, 3.2, 3.3, 3.4, Django 1.5+.
 
 
 Installation
--- tests/__init__.py
+++ tests/__init__.py
@@ -22,6 +22,7 @@ except ImportError:
 test_runner = None
 old_config = None
 
+
 def setup():
     global test_runner
     global old_config
@@ -29,6 +30,7 @@ def setup():
     test_runner = TestSuiteRunner()
     test_runner.setup_test_environment()
     old_config = test_runner.setup_databases()
+
 
 def teardown():
     test_runner.teardown_databases(old_config)
