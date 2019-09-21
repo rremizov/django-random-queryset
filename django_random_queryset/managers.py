57e8acb9fd46385c68adc4cd4d6c13c63a9b3cc9
rremizov <rremizov@yandex.ru>
2019-09-21 17:22:41 +0500

    Cleanup

--- django_random_queryset/managers.py
+++ django_random_queryset/managers.py
@@ -8,12 +8,7 @@ from . import queryset
 
 class RandomManager(models.Manager):
     def random(self, *args, **kwargs):
-        return self.__get_queryset().random(*args, **kwargs)
+        return self.get_queryset().random(*args, **kwargs)
 
-    def __get_queryset(self):
+    def get_queryset(self):
         return queryset.RandomQuerySet(self.model)
-
-    if django.VERSION[0] == 1 and django.VERSION[1] < 7:
-        get_query_set = __get_queryset
-    else:
-        get_queryset = __get_queryset
