29acd8ffecdddd568473f28230116e9e03bb4cf7
rremizov <rremizov@yandex.ru>
2019-09-21 17:18:53 +0500

    Add django 2.2 support

--- setup.py
+++ setup.py
@@ -5,22 +5,14 @@ setup(
     version='0.1.1',
     author='Roman M. Remizov',
     author_email='rremizov@yandex.ru',
-
     license='MIT',
     platforms=['any'],
     description="The extension gives you ability to pull random records using Django's ORM.",
     long_description=open('README.rst').read(),
     url='http://github.com/rremizov/django-random-queryset',
-
-    packages=[
-        'django_random_queryset',
-    ],
-    install_requires=[
-        'django>=1.11',
-    ],
-
+    packages=['django_random_queryset'],
+    install_requires=['django>=1.11'],
     test_suite='tests.run_tests.run_all',
-
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
@@ -29,21 +21,16 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Framework :: Django',
-        'Framework :: Django :: 1.7',
-        'Framework :: Django :: 1.8',
-        'Framework :: Django :: 1.9',
-        'Framework :: Django :: 1.10',
         'Framework :: Django :: 1.11',
         'Framework :: Django :: 2.0',
         'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ]
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
