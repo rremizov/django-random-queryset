8dd312173fe6736e3281afc7c8793cc032fef08c
Roman Remizov <rremizov@yandex.ru>
2020-01-19 16:16:56 +0500

    Require Django>=1.11.27 [CVE-2019-19844]

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     long_description=open('README.rst').read(),
     url='http://github.com/rremizov/django-random-queryset',
     packages=['django_random_queryset'],
-    install_requires=['django>=1.11'],
+    install_requires=['django>=1.11.27'],
     test_suite='tests.run_tests.run_all',
     classifiers=[
         'Development Status :: 3 - Alpha',
